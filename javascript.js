3f4f4b4e6fa4749bdecce7d8439169d8591aaac5
Alex Bennett <kestralttr@gmail.com>
2017-05-06 22:37:33 -0700

    adds arrow functionality

--- javascript.js
+++ javascript.js
@@ -14,6 +14,8 @@ Array.prototype.forEach.call(thumbnailList,function(el,idx) {
   el.setAttribute("data-candle-name",candleArr[idx]);
   el.addEventListener("click",function() {
     currentCandle = this.getAttribute("data-candle-name");
+    currentCandleIdx = candleArr.indexOf(currentCandle);
+    console.log(currentCandleIdx);
     activationObj[currentCandle](candleArr[idx]);
   });
 });
@@ -21,7 +23,6 @@ Array.prototype.forEach.call(thumbnailList,function(el,idx) {
 let updateThumbnails = function() {
   thumbnailList = document.getElementsByClassName("thumbnail-item");
   Array.prototype.forEach.call(thumbnailList,function(el,idx) {
-    console.log(currentCandle);
     if(el.getAttribute("data-candle-name") === currentCandle) {
       el.classList.add("active-thumbnail");
     } else {
@@ -30,11 +31,31 @@ let updateThumbnails = function() {
   });
 };
 
-let activeThumbnail;
+
+let leftArrow = document.getElementById("left-arrow-section");
+leftArrow.addEventListener("click", function() {
+  if(currentCandleIdx <= 0) {
+    currentCandleIdx = candleArr.length - 1;
+  } else {
+    currentCandleIdx--;
+  }
+  currentCandle = candleArr[currentCandleIdx];
+  activationObj[currentCandle]();
+});
+
+let rightArrow = document.getElementById("right-arrow-section");
+rightArrow.addEventListener("click", function() {
+  if(currentCandleIdx >= candleArr.length-1) {
+    currentCandleIdx = 0;
+  } else {
+    currentCandleIdx++;
+  }
+  currentCandle = candleArr[currentCandleIdx];
+  activationObj[currentCandle]();
+});
+
 let activationObj = {
-  "Tomato Leaf": function(candleName) {
-    currentCandle = candleName;
-    console.log(currentCandle);
+  "Tomato Leaf": function() {
     $("body").css("background-image","url('images/candles/tomatoleaf/bg1.jpg')");
     $("#product-image").css("background-image","url('images/tl1.png')");
     $("#product-text p").text("Bring the beautiful simplicity of nature into your home with the natural freshness of the tomato leaf.");
@@ -42,9 +63,7 @@ let activationObj = {
     $("#product-title").css("color","rgb(49,165,84)");
     updateThumbnails();
   },
-  "Gasoline": function(candleName) {
-    currentCandle = candleName;
-    console.log(currentCandle);
+  "Gasoline": function() {
     $("body").css("background-image","url('')");
     $("#product-image").css("background-image","url('')");
     $("#product-text p").text("Sting the nostrils with the smell of refined petroleum to your heart's content.  Brain damage free.");
@@ -52,9 +71,7 @@ let activationObj = {
     $("#product-title").css("color","orange");
     updateThumbnails();
   },
-  "Blueberry": function(candleName) {
-    currentCandle = candleName;
-    console.log(currentCandle);
+  "Blueberry": function() {
     $("body").css("background-image","url('')");
     $("#product-image").css("background-image","url('')");
     $("#product-text p").text("Earth's most famous blue fruit gave its life for this lovely candle.");
@@ -62,9 +79,7 @@ let activationObj = {
     $("#product-title").css("color","blue");
     updateThumbnails();
   },
-  "Baby Powder": function(candleName) {
-    currentCandle = candleName;
-    console.log(currentCandle);
+  "Baby Powder": function() {
     $("body").css("background-image","url('')");
     $("#product-image").css("background-image","url('')");
     $("#product-text p").text("No longer need you have another child as an excuse to keep buying baby powder.  Fill your almost empty house with its fragrance, commitment free.");
@@ -72,9 +87,7 @@ let activationObj = {
     $("#product-title").css("color","rgb(120,120,120)");
     updateThumbnails();
   },
-  "Skunk": function(candleName) {
-    currentCandle = candleName;
-    console.log(currentCandle);
+  "Skunk": function() {
     $("body").css("background-image","url('')");
     $("#product-image").css("background-image","url('')");
     $("#product-text p").text("Some people dream about the scent of skunk.  Whoever you are, you're really weird, but we've got you covered.");
@@ -84,4 +97,4 @@ let activationObj = {
   },
 };
 
-activationObj[currentCandle](candleArr[0]);
+activationObj[currentCandle]();
